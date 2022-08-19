59f3daf95f930c24fee0005207d8ca6dcaadae32
magarb98 <magarb98@users.noreply.github.com>
2022-08-19 20:41:41 +0100

    image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img src="/troll.png" className="header--image" />
+      <img src="troll.png" className="header--image" />
       <h2 className="header--title">Header component</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
